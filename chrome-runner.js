3b2ca949e3ffeb2448bedb7966b349945080c0f0
Eryk Napierala <eryk.piast@gmail.com>
2019-12-09 21:54:49 +0100

    Name unnamed

--- chrome-runner.js
+++ chrome-runner.js
@@ -1,17 +1,19 @@
 const puppeteer = require('puppeteer');
 
+function getMetrics() {
+    return {
+        timing: window.performance.timing.toJSON(),
+        paint: performance.getEntriesByType('paint').map(mark => mark.toJSON()),
+        mark: performance.getEntriesByType('mark').map(mark => mark.toJSON())
+    };
+}
+
 module.exports = async function launch(url) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: 'networkidle2' });
 
-    const results = await page.evaluate(() => {
-        return {
-            timing: window.performance.timing.toJSON(),
-            paint: performance.getEntriesByType('paint').map(mark => mark.toJSON()),
-            mark: performance.getEntriesByType('mark').map(mark => mark.toJSON())
-        };
-    });
+    const results = await page.evaluate(getMetrics);
 
     await page.close();
     await browser.close();
